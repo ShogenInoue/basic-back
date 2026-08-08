0f5f431df38c812b0e1e2cf4e48cc4e3dd2ef063
Shlo <161896702+ShogenInoue@users.noreply.github.com>
2026-08-08 14:13:08 -0500

    Add TLS proxy server functionality
    Implement a proxy server that handles TLS client hello messages and forwards them to the appropriate backend server.

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
+const net = require('net');
+const { parseTLSClientHello } = require('read-tls-client-hello');
 
+const proxyServer = net.createServer((clientSocket) => {
+  clientSocket.once('data', (initalData) => {
+    try {
+      const parsedData = parseTLSClientHello(initalData);
+      const serversName = parsedData.serverName;
+
+      const backendSocket = net.connect(443, serversName () =>{
+        autoPiping(clientSocket, backendSocket, initalData);
+        });
+      backendSocket.on('error', (err) => {
+        console.error((err));
+        clientSocket.destory();
+      });
+    } catch (error) {
+      console.error(error.message);
+      clientSocket.destory();
+    };
+      
+  });
+});
+
+function autoPiping(client, backend, hello) {
+  backend.write(hello);
+
+  client.pipe(backend);
+  backend.pipe(client);
+
+  client.on('error', (err) => {
+    console.error(err);
+    client.destory();
+  });
+  backend.on('error', (err) => {
+    console.error(err);
+    backend.destory();
+  });
+}
+
+proxyServer.listen(433, () => {
+  console.log('Started');
+});
