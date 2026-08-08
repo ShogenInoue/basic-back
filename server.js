65344afb84d9d48143b78fd0cbb58af7da96b8fe
Shlo <161896702+ShogenInoue@users.noreply.github.com>
2026-08-08 14:49:09 -0500

    Replace parseTlsClientHello with readTlsClientHello

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const net = require('net');
-const { parseTlsClientHello } = require('read-tls-client-hello');
+const { readTlsClientHello } = require('read-tls-client-hello');
 
 const proxyServer = net.createServer((clientSocket) => {
   clientSocket.once('data', (initalData) => {
     try {
-      const parsedData = parseTlsClientHello(initalData);
+      const parsedData = readTlsClientHello(initalData);
       const serversName = parsedData.serverName;
 
       const backendSocket = net.connect(443, serversName, () => {
